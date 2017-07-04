fb7b1fc9c698ea70d569fceea3f2dd82bc4c9ec0
mneli <m.nyeem@elium.academy>
2017-07-04 09:52:14 +0200

    add WelcomeActivity and basic layout

--- app/src/main/java/be/mneli/rushhour/WelcomeActivity.java
+++ app/src/main/java/be/mneli/rushhour/WelcomeActivity.java
@@ -1,13 +1,58 @@
 package be.mneli.rushhour;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnWelcomeLogin;
+    private Button btnWelcomeRegister;
+    private Button btnWelcomeGuest;
+    private EditText etWelcomeMail;
+    private EditText etWelcomePassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        initView();
+    }
+
+    private void initView() {
+        btnWelcomeLogin = (Button) findViewById(R.id.btn_welcome_login);
+        btnWelcomeRegister = (Button) findViewById(R.id.btn_welcome_register);
+        btnWelcomeGuest = (Button) findViewById(R.id.btn_welcome_guest);
+        etWelcomeMail = (EditText) findViewById(R.id.et_welcome_username);
+        etWelcomePassword = (EditText) findViewById(R.id.et_welcome_password);
+
+        btnWelcomeLogin.setOnClickListener(this);
+        btnWelcomeRegister.setOnClickListener(this);
+        btnWelcomeGuest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_welcome_login:
+                //TODO
+                break;
+            case R.id.btn_welcome_register:
+                //TODO
+                break;
+            case R.id.btn_welcome_guest:
+                goToActivity(HomeActivity.class);
+                break;
+            default:
+        }
+    }
+
+    private void goToActivity(Class activity) {
+        Intent callIntent = new Intent(this, activity);
+        startActivity(callIntent);
     }
 }
