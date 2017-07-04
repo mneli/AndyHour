7a94c10b79bea71d3e4563f52ebdbff9b6962dc5
mneli <m.nyeem@elium.academy>
2017-07-04 10:37:05 +0200

    add HomeActivity & basic layout

---
+++ app/src/main/java/be/mneli/rushhour/HomeActivity.java
@@ -0,0 +1,70 @@
+package be.mneli.rushhour;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnHomeGame;
+    private Button btnHomeScores;
+    private Button btnHomeWiki;
+    private Button btnHomeSettings;
+    private TextView tvHomeCity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        initView();
+    }
+
+    private void initView() {
+        btnHomeGame = (Button) findViewById(R.id.btn_home_game);
+        btnHomeScores = (Button) findViewById(R.id.btn_home_scores);
+        btnHomeWiki = (Button) findViewById(R.id.btn_home_wiki);
+        btnHomeSettings = (Button) findViewById(R.id.btn_home_settings);
+        tvHomeCity = (TextView) findViewById(R.id.tv_home_city);
+
+        btnHomeGame.setOnClickListener(this);
+        btnHomeScores.setOnClickListener(this);
+        btnHomeWiki.setOnClickListener(this);
+        btnHomeSettings.setOnClickListener(this);
+
+        setCityName();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_home_game:
+                //TODO
+                break;
+            case R.id.btn_home_scores:
+                //TODO
+                break;
+            case R.id.btn_home_wiki:
+                //TODO
+                break;
+            case R.id.btn_home_settings:
+                //TODO
+                break;
+            default:
+        }
+    }
+
+    private void goToActivity(Class activity) {
+        Intent callIntent = new Intent(this, activity);
+        startActivity(callIntent);
+    }
+
+    private String getCityName() {
+        return getString(R.string.default_city_name);
+    }
+
+    private void setCityName() {
+        tvHomeCity.setText(getCityName());
+    }
+}
