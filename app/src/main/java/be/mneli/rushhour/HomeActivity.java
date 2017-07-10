b1af1c8cb299c1ae6a172adc5ff62e4c7dfbe4f2
mneli <m.nyeem@elium.academy>
2017-07-10 04:54:14 +0200

    add get lat/long

--- app/src/main/java/be/mneli/rushhour/HomeActivity.java
+++ app/src/main/java/be/mneli/rushhour/HomeActivity.java
@@ -1,12 +1,22 @@
 package be.mneli.rushhour;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnHomeGame;
@@ -15,11 +25,37 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnHomeSettings;
     private TextView tvHomeCity;
 
+    private FusedLocationProviderClient mFusedLocationClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLocation();
         initView();
+    }
+
+    private void getLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            }
+        }
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            Log.e("LOCATION_GET_LOCATION", location.toString());
+                        }
+                    }
+                });
     }
 
     private void initView() {
